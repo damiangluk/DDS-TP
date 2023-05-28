04916b09ff8f6543860f239f0b74088e501b3cf1
Nicolas Goldfarb <nicolasgol2016@gmail.com>
2023-05-28 16:19:09 -0300

    cambio segunda carpeta

---
+++ TPINTEGRADOR/TPINTEGRADOR/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics;
+using TPINTEGRADOR.Models;
+
+namespace TPINTEGRADOR.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        { 
+            _logger = logger;
+        }
+        #region routes
+
+        public IActionResult Login()
+        {
+            ViewBag.ContraseniaSugerida = Validador.GetVerificador().GenerarContraseniaSugerida();
+
+			return View();
+        }
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        #endregion
+
+        [HttpPost]
+		public IActionResult Login(LoginViewModel model)
+		{
+            (bool, string) resultado = Validador.GetVerificador().EsClaveSegura(model.Password);
+			if (resultado.Item1)
+			{
+				return RedirectToAction("Index", "Home");
+			}
+			else
+			{
+				ViewBag.ContraseniaSugerida = Validador.GetVerificador().GenerarContraseniaSugerida();
+				ViewBag.MensajeError = resultado.Item2;
+				return View("login", model);
+			}
+        }
+    }
+}
