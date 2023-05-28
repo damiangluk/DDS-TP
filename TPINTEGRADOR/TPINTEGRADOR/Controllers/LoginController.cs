c2e790ade0673131eb317a4c57b6478deffef3cd
Nicolas Goldfarb <nicolasgol2016@gmail.com>
2023-05-28 16:15:21 -0300

    Entrega 1

--- TPINTEGRADOR/TPINTEGRADOR/Controllers/LoginController.cs
+++ TPINTEGRADOR/TPINTEGRADOR/Controllers/LoginController.cs
@@ -10,7 +10,7 @@ namespace TPINTEGRADOR.Controllers
         private readonly ILogger<LoginController> _logger;
 
         public LoginController(ILogger<LoginController> logger)
-        {
+        { 
             _logger = logger;
         }
         #region routes
