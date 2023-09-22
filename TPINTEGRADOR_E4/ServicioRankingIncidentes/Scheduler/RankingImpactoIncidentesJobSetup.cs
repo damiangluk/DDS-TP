a546bc798c06cca4e6b2bcd4d1371e1d34c9ec8b
damiangluk <damigluk@gmail.com>
2023-09-22 00:46:37 -0300

    agrego configuracion para once a week

--- TPINTEGRADOR_E4/ServicioRankingIncidentes/Scheduler/RankingImpactoIncidentesJobSetup.cs
+++ TPINTEGRADOR_E4/ServicioRankingIncidentes/Scheduler/RankingImpactoIncidentesJobSetup.cs
@@ -11,6 +11,8 @@ namespace ServicioRankingIncidentes.Scheduler
             options
                 .AddJob<RankingImpactoIncidentesJob>(jobBuilder => jobBuilder.WithIdentity(jobKey))
                 .AddTrigger(trigger => trigger.ForJob(jobKey).WithSimpleSchedule(schedule => schedule.WithIntervalInSeconds(5).RepeatForever()));
+
+            //.WithSchedule(CronScheduleBuilder.CronSchedule("0 0 9 ? * MON"))
         }
     }
 }
